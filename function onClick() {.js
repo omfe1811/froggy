237a8fe92cd024f85cd35202bd5d5dbf2fbc5fab
omfe1811 <omfe1811@gmail.com>
2024-11-21 13:46:30 +0100

    git bullshit

---
+++ function onClick() {.js
@@ -0,0 +1,9 @@
+function onClick() {
+    clicks++;
+    document.getElementById("clicks").innerHTML = clicks;
+    Image.show('/home/big_polish_daddy69/Desktop/kurset');
+};
+
+function worm() {
+
+};
